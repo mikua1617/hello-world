2dbe184a9ce1f8f85220d14ca62e70ad3cacf962
mikua1617 <35597661+mikua1617@users.noreply.github.com>
2018-02-20 00:28:05 +0530

    Update W4_problem_set_4.py

--- W4_problem_set_4.py
+++ W4_problem_set_4.py
@@ -54,3 +54,85 @@ def calculateHandlen(hand):
         length += hand[counter]
 
     return length
+
+  
+#problem 5 -   Playing a Hand
+  
+def playHand(hand, wordList, n):
+    
+
+    # Keep track of the total score
+    score=0
+    # As long as there are still letters left in the hand:
+    while len(hand)>0:
+
+
+        # Display the hand
+        displayHand(hand)
+        # Ask user for input
+        word=input("enter word or a" + " '.' " + "to indicate that you're finished: ")
+        # If the input is a single period:
+        if word==".":
+
+            break
+            # End the game (break out of the loop)
+
+        else:
+
+
+        # Otherwise (the input is not a single period):
+
+            # If the word is not valid:
+            if isValidWord(word, hand, wordList)==False:
+
+                # Reject invalid word (print a message followed by a blank line)
+                print("invalid word! please try again.")
+
+            # Otherwise (the word is valid):
+            else:
+                score+=getWordScore(word, n)
+                print(word, "earned", getWordScore(word, n), "points,", "Total:", score, "points")
+                # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
+
+                # Update the hand 
+                hand=updateHand(hand, word)
+
+    # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+    print("Game Over. Total score=", score)
+    print(" ")
+    
+    
+# Problem 6 - playing a game
+
+def playGame(wordList):
+    """
+
+    Allow the user to play an arbitrary number of hands.
+
+    1) Asks the user to input 'n' or 'r' or 'e'.
+      * If the user inputs 'n', let the user play a new (random) hand.
+      * If the user inputs 'r', let the user play the last hand again.
+      * If the user inputs 'e', exit the game.
+      * If the user inputs anything else, tell them their input was invalid.
+ 
+    2) When done playing the hand, repeat from step 1    
+    """
+    gamechoose="a"
+
+    while gamechoose!="e":
+
+
+        gamechoose=input("enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if gamechoose == "n":
+            hand=dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+        elif gamechoose == "r":
+            playHand(hand, wordList, HAND_SIZE)
+        elif gamechoose == "e":
+            print("thank you for playing!")
+        else:
+            print("invalid entry, please try again")
+
+
+
+
