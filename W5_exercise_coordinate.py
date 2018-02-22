3e70e5eed02e4dbacd8394b3c4f021c6c6b7c0a7
mikua1617 <35597661+mikua1617@users.noreply.github.com>
2018-02-22 11:54:04 +0530

    Create W5_exercise_coordinate.py

---
+++ W5_exercise_coordinate.py
@@ -0,0 +1,10 @@
+
+def __eq__(self, other):
+        if self.getX()==other.getX() and self.getY()==other.getY():
+            return True
+        else:
+            return False
+            
+    def __repr__(self):
+        return "Coordinate(%d, %d)" % (self.getX(), self.getY())
+
