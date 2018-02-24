d7b39cfd1724285e89777d93d302d8901327dd51
mikua1617 <35597661+mikua1617@users.noreply.github.com>
2018-02-25 01:57:08 +0530

    Create W5_exercise_genprimes.py

---
+++ W5_exercise_genprimes.py
@@ -0,0 +1,15 @@
+def genPrimes():
+    primes=[2]
+    prime=2
+    flag=0
+    while True:
+        prime+=1
+        flag=0
+        for i in primes:
+            if prime%i==0:
+                flag=1
+                break
+
+        if flag==0:
+            primes.append(prime)
+            yield primes
